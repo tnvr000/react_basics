90a56e298409306568d7d388f5fc19dbeb7c42a9
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2019-03-07 11:57:05 +0530

    completed lists and keys

---
+++ js/08_lists_and_keys.js
@@ -0,0 +1,20 @@
+"use strict";
+window.onload = function() {
+	const rootContainer = document.getElementById('root');
+	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	ReactDOM.render(NumberList({numbers: numbers}), rootContainer)
+}
+
+function NumberList(props) {
+	const numbers = props.numbers;
+	const listItems = numbers.map((number)=>{
+		return React.createElement(
+			'li',
+			{
+				key: number.toString()
+			},
+			number
+		);
+	})
+	return React.createElement('ul', null, listItems)
+}
